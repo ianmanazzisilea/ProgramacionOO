35ca53f0912a6c9833d1eba68727df15b463f1f8
ianmanazzisilea <143664080+ianmanazzisilea@users.noreply.github.com>
2023-08-31 19:38:10 -0300

    Update Lista.java

--- ar/edu/unlu/poo/tp1/ejercicio1/Lista.java
+++ ar/edu/unlu/poo/tp1/ejercicio1/Lista.java
@@ -1,90 +1,145 @@
 package ar.edu.unlu.poo.tp1.ejercicio1;
 
 
-import static java.util.Objects.isNull;
-
 public class Lista {
     private Nodo nodocero;
     public boolean esVacia(){
-        if (nodocero.getSiguiente() == null){
+        if (nodocero == null){
             return true;
         }
         else return false;
     }
     public void agregar(Object dato){
-        Nodo nodosiguiente = new Nodo();
-        nodosiguiente=nodocero;
+        Nodo nodo = new Nodo();
+        nodo=nodocero;
         Nodo nuevonodo = new Nodo();
         nuevonodo.setDato(dato);
-        while (nodosiguiente.getSiguiente() != null){
-            nodosiguiente = nodosiguiente.getSiguiente();
+        if (nodocero == null){
+            nodocero = nuevonodo;
         }
-        nodosiguiente.setSiguiente(nuevonodo);
-
+        else{
+            while (nodo.getSiguiente() != null){
+                nodo = nodo.getSiguiente();
+            }
+            nodo.setSiguiente(nuevonodo);
+        }
     }
     public String longitud(){
-        Nodo nodosiguiente = nodocero;
+        Nodo nodo = nodocero;
         int i=0;
-        while (!isNull(nodosiguiente.getSiguiente())){
-            nodosiguiente = nodosiguiente.getSiguiente();
+        if (nodo!=null){
             i++;
+            while (nodo.getSiguiente() != null){
+                nodo = nodo.getSiguiente();
+                i++;
+            }
         }
         return String.valueOf(i);
     }
     public boolean eliminar(int elemento){
         Nodo nodo = nodocero;
-        int i=0;
-        while (!isNull(nodo.getSiguiente())){
-            if (i< (elemento - 1)){
-                nodo = nodo.getSiguiente();
-                i++;
-            }
-            else
-                if (i == (elemento - 1)){
-                    nodo.setSiguiente(nodo.getSiguiente().getSiguiente());
+        Nodo nodosiguiente = new Nodo();
+        int i=1;
+        if (nodo != null){
+            if (nodo.getSiguiente() != null){
+                while (nodo.getSiguiente() != null){
+                    if (i< (elemento )){
+                        nodo = nodo.getSiguiente();
+                        i++;
+                    }
+                    else
+                    if (i == (elemento  )){
+                        nodosiguiente = nodo.getSiguiente();
+                        nodo.setSiguiente(nodosiguiente.getSiguiente());
+                        nodo.setDato(nodosiguiente.getDato());
+                        nodosiguiente = null;
+                        return true;
+                    }
+                }
+                if (i == (elemento )){
+                    nodo = null;
                     return true;
                 }
+                else return false;
+            }
+            if (elemento == 1){
+                nodocero = null;
+                return true;
+            }
+            else return false;
         }
-        return false;
+        else return false;
     }
     public Object recuperar(int elemento){
         Nodo nodo = nodocero;
-        int i=0;
-        while (nodo.getSiguiente() != null){
-            if (i< (elemento)){
-                nodo = nodo.getSiguiente();
-                i++;
+        int i=1;
+        if (nodo != null){
+            if (nodo.getSiguiente() != null){
+                while (nodo.getSiguiente() != null){
+                    if (i< (elemento)){
+                        nodo = nodo.getSiguiente();
+                        i++;
+                    }
+                    else
+                        if (i == (elemento)){
+                            return nodo.getDato();
+                        }
+                }
+                return null;
             }
             else
-            if (i == (elemento)){
-                return nodo.getDato();
-            }
+                if (elemento == 1){
+                    return nodocero.getDato();
+                }
+                else return null;
         }
-        return false;
+        else return nodocero.getDato();
+
     }
-    public boolean inertar(Object elemento, int posicion){
+    public boolean insertar(Object elemento, int posicion){
         Nodo nodo = nodocero;
         int i=0;
-        while (!isNull(nodo.getSiguiente())){
-            if (i< (posicion - 1)){
-                nodo = nodo.getSiguiente();
-                i++;
+        Nodo nuevonodo = new Nodo();
+        nuevonodo.setDato(elemento);
+        if (nodo != null){
+            if (nodo.getSiguiente() != null){
+                while (nodo.getSiguiente() != null){
+                    if (i< (posicion - 1)){
+                        nodo = nodo.getSiguiente();
+                        i++;
+                    }
+                    else
+                    if (i == (posicion - 1)){
+                        nuevonodo.setSiguiente(nodo.getSiguiente());
+                        nodo.setSiguiente(nuevonodo);
+                        return true;
+                    }
+                }
             }
             else
-            if (i == (posicion - 1)){
-                Nodo nuevonodo = new Nodo();
-                nuevonodo.setDato(elemento);
-                nuevonodo.setSiguiente(nodo.getSiguiente());
-                nodo.setSiguiente(nuevonodo);
-                return true;
-            }
+                if (posicion == 2){
+                    nodo.setSiguiente(nuevonodo);
+                }
+                else
+                    if (posicion == 1){
+                        nuevonodo.setSiguiente(nodo);
+                        nodocero = nuevonodo;
+                    }
         }
+        else
+            if (posicion == 1){
+                nodo.setDato(elemento);
+            }
+
         return false;
     }
     public void mostrar(){
         Nodo nodo = nodocero;
-        int i=0;
-        while (nodo.getSiguiente() != null) {
+        if (nodo != null){
+            while (nodo.getSiguiente() != null) {
+                System.out.println(nodo.getDato());
+                nodo = nodo.getSiguiente();
+            }
             System.out.println(nodo.getDato());
         }
     }
