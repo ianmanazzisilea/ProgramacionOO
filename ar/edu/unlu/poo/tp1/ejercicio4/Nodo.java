07a3d9f417897a63e85d3b80bfebafbd30ca7234
ianmanazzisilea <143664080+ianmanazzisilea@users.noreply.github.com>
2023-08-31 23:33:48 -0300

    Add files via upload

---
+++ ar/edu/unlu/poo/tp1/ejercicio4/Nodo.java
@@ -0,0 +1,22 @@
+package ar.edu.unlu.poo.tp1.ejercicio4;
+
+public class Nodo {
+    private Object dato=null;
+    private Nodo siguiente=null;
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Object getDato() {
+        return this.dato;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+}
