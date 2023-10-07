4424ebe5fffc358f75bf58ef804813c8b60224ff
ianmanazzisilea <143664080+ianmanazzisilea@users.noreply.github.com>
2023-10-07 10:08:44 -0300

    Delete ar/edu/unlu/poo/tp2/Ejercicio-9 directory

--- ar/edu/unlu/poo/tp2/Ejercicio-9/Figura3d.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.unlu.poo.tp2.ej9;
-
-public abstract class Figura3d extends FiguraGeometrica{
-    abstract double getVolumen();
-}
--- ar/edu/unlu/poo/tp2/Ejercicio-9/FiguraGeometrica.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.unlu.poo.tp2.ej9;
-
-public abstract class FiguraGeometrica {
-    public abstract double getArea();
-}
