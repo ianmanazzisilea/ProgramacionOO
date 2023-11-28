4f4a3abeda98356f241e29aa4ad5f2cceaed6c71
ianmanazzisilea <143664080+ianmanazzisilea@users.noreply.github.com>
2023-11-28 10:33:48 -0300

    Delete ar/edu/unlu/poo/tpfinal directory

--- ar/edu/unlu/poo/tpfinal/IVista.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.unlu.poo.tpfinal;
-
-public interface IVista {
-    void mesaactualizada();
-
-}
--- ar/edu/unlu/poo/tpfinal/VistaGrafica.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unlu.poo.tpfinal;
-
-public class VistaGrafica implements IVista{
-    @Override
-    public void mesaactualizada() {
-
-    }
-}
